f2286deabdcbcb11a094d3e1e0e61d38fb369a0a
Dave Hurst <david@hurst.com>
2025-01-26 18:09:35 -0800

    update terraform file

--- homework/h1-terra/variables.tf
+++ homework/h1-terra/variables.tf
@@ -1,20 +1,18 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
-  #ex: if you have a directory where this file is called keys with your service account json file
-  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+  default     = "/Users/davidhurst/Documents/zoomcamp-data-eng/zoomcamp-data-eng-903c3c5f2174.json"
 }
 
 
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "zoomcamp-data-eng"
 }
 
 variable "region" {
   description = "Region"
   #Update the below to your desired region
-  default     = "us-central1"
+  default     = "us-west2"
 }
 
 variable "location" {
@@ -26,13 +24,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   #Update the below to what you want your dataset to be called
-  default     = "demo_dataset"
+  default     = "m1_dataset"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "m1-terra-bucket"
 }
 
 variable "gcs_storage_class" {
